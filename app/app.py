83f16cf37bda59e8c2f8d8b15e6b220d8a6903b6
Nikita Yanchenko <maraabus@mail.ru>
2025-05-14 21:11:38 +0700

    'тестим2'

--- app/app.py
+++ app/app.py
@@ -78,5 +78,6 @@ def trigger_reset():
 
 
 
-if __name__ == '__main__':
-    app.run(port=5000, debug=True, use_reloader=False)  # Отключаем reloader!
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)

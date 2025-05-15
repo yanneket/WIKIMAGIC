0858b15b6df9b9328b9f0b1c6b1b56b3a2d2dc25
Nikita Yanchenko <maraabus@mail.ru>
2025-05-15 07:09:25 +0700

    'adsa'

--- app/app.py
+++ app/app.py
@@ -5,7 +5,13 @@ from datetime import datetime, timedelta
 from threading import Lock
 import os
 
-app = Flask(__name__)
+BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+
+app = Flask(
+    __name__,
+    template_folder=os.path.join(BASE_DIR, 'templates'),
+    static_folder=os.path.join(BASE_DIR, 'static')
+)
 
 # Хранилище сбросов
 active_resets = {}
